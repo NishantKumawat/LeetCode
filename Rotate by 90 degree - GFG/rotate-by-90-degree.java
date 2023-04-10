f94bc16901b40430e528d635eefeb0e0b9fecd9b
Nishant Kumawat <97976136+NishantKumawat@users.noreply.github.com>
2023-04-10 09:53:09 +0530

    Added solution - LeetHub

--- Rotate by 90 degree - GFG/rotate-by-90-degree.java
+++ Rotate by 90 degree - GFG/rotate-by-90-degree.java
@@ -44,16 +44,6 @@ class Solution
     //Function to rotate matrix anticlockwise by 90 degrees.
     static void rotateby90(int matrix[][], int n) 
     { 
-        // code here
-        // for(int row = 0;row<matrix.length;row++){
-        //     for(int col =0;col<matrix[0].length;col++){
-        //         if(col<n-col-1){
-        //             int temp = matrix[row][col];
-        //             matrix[row][col] = matrix[row][n-col-1];
-        //             matrix[row][n-col-1] = temp;
-        //         }
-        //     }
-        // }
         for(int row = 0;row<matrix.length;row++){
             for(int col =0;col<matrix[0].length;col++){
                 if(col<row){
@@ -75,18 +65,3 @@ class Solution
         }
     }
 }
-
-// 3 6 9
-// 2 5 4
-// 1 8 7
-
-
-// 1 2 3
-// 4 5 6 
-// 7 8 9
-
-
-// 1 4 7
-// 2 5 8
-// 3 6 9
-
